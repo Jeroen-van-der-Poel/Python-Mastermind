3c8857f084aa00c2208600f5b572f6eee20e48e8
Snowborn <jfa.vanderpoel@student.avans.nl>
2020-03-30 14:56:35 +0200

    database

---
+++ MasterMind/db_connection.py
@@ -0,0 +1,37 @@
+import os
+import sqlite3
+
+db_name = 'mastermind.db'
+path = os.path.dirname(os.path.abspath(__file__))
+
+
+class db_connection:
+
+    @staticmethod
+    def select_query(query: str, data=None):
+        conn = sqlite3.connect(os.path.join(path, db_name))
+        cursor = conn.cursor()
+        if data:
+            cursor.execute(query, data)
+        else:
+            cursor.execute(query)
+        returnable = cursor.fetchall()
+        conn.close()
+        return returnable
+
+    @staticmethod
+    def query(query: str, data=None):
+        conn = sqlite3.connect(os.path.join(path, db_name))
+        cursor = conn.cursor()
+        if data:
+            cursor.execute(query, data)
+        else:
+            cursor.execute(query)
+        conn.commit()
+        conn.close()
+
+    @staticmethod
+    # destructor
+    def __del__(cls):
+        conn = sqlite3.connect(os.path.join(path, db_name))
+        conn.close()
