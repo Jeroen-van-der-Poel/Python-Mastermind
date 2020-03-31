ab5f3a9bd742ce99c3a10ee613e89cc321cb9664
Snowborn <jfa.vanderpoel@student.avans.nl>
2020-03-31 14:12:26 +0200

    iets

--- MasterMind/App.py
+++ MasterMind/App.py
@@ -1,9 +1,25 @@
-from flask import Flask, render_template, url_for
+import logging
+import os
+
+from flask import Flask, render_template, url_for, session, request, redirect
+from Player import Player
+from db_connection import db_connection
+
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+app.logger.setLevel(logging.INFO)
+
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
+def home():
+    return render_template('login.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if 'player' in session:
+        return render_template('game.html')
+    if request.method == 'POST':
+        username = request.form['user']
+        Player(username)
+        return redirect(url_for('profile'))
+    return render_template('login.html')
---
+++ MasterMind/Player.py
@@ -0,0 +1,14 @@
+from flask import session
+
+from db_connection import db_connection
+
+
+class Player:
+    def __init__(self, username: str):
+        if 'player' not in session:
+            session['player'] = username
+            rows = db_connection.select_query('SELECT * FROM User WHERE name = ?', [username])
+            if len(rows) == 0:
+                db_connection.query('INSERT INTO User VALUES(null, ?)', [username])
+                rows = db_connection.select_query('SELECT * FROM User WHERE name = ?', [username])
+            session['player_id'] = rows[0][0]
