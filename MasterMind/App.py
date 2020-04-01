ed031162c5c2e6c8aa00a3c75b1e43d5b9415d8e
Bas van Pelt <49717976+Basje265@users.noreply.github.com>
2020-04-01 11:56:18 +0200

    stats

--- MasterMind/App.py
+++ MasterMind/App.py
@@ -38,6 +38,18 @@ def game():
         return render_template('gamestart.html')
     return render_template('login.html')
 
+@app.route('/statistics/')
+def stats():
+    if 'player' in session:
+        d1 = db_connection.select_query('SELECT AVG(turns) FROM Game WHERE player_id = ? AND is_finished = true', [session['player_id']])[0][0]
+        d2 = db_connection.select_query('SELECT COUNT(player_id) FROM Game WHERE player_id = ? AND is_finished = true', [session['player_id']])[0][0]
+        d3 = db_connection.select_query('SELECT COUNT(*) - (SELECT COUNT(player_id) FROM games WHERE player_id = ? '
+                                        'AND is_finished = true) FROM Game WHERE player_id = ?', [session['player_id'], session['player_id']])[0][0]
+        d4 = db_connection.select_query('SELECT created_at FROM Game WHERE player_id = ? order by '
+                                        'created_at LIMIT 1', [session['player_id']])[0][0]
+        return render_template('statistics.html', db_connection=db_connection, d1=d1, d2=d2, d3=d3, d4=d4)
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run()
